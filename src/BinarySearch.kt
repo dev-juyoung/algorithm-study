9765a21b64073dfc1cbfacff99cb0b9fe19f7f78
Juyoung, Lee <dev.juyoung@gmail.com>
2018-04-11 20:11:46 +0900

    이진탐색 알고리즘 구현.

---
+++ src/BinarySearch.kt
@@ -0,0 +1,28 @@
+class BinarySearch {
+    fun implemented(list: Array<Int>, item: Int): Int? {
+        var low = 0
+        var high = list.size - 1
+
+        while (low <= high) {
+            val mid = (low + high) / 2
+            val guess = list[mid]
+
+            if (guess == item) return mid
+
+            if (guess > item) {
+                high = mid - 1
+            } else {
+                low = mid + 1
+            }
+        }
+
+        return null
+    }
+
+    fun execute() {
+        val source = arrayOf(1, 3, 5, 7, 8)
+
+        println("탐색 결과: ${implemented(source, 3)}")
+        println("탐색 결과: ${implemented(source, -1)}")
+    }
+}
